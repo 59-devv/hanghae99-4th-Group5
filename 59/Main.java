3fe6fe68d54d94fd04d264451033325b5ee38470
59 <5.9.dev@gmail.com>
2021-10-21 12:54:58 +0900

    Second Test

---
+++ 59/Main.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int num = Integer.parseInt(st.nextToken());
+        int temp = num;
+        int cnt = 0;
+        while (true) {
+            int target = ((num%10)*10)+(((num/10)+(num%10))%10);
+            cnt++;
+            if (target == temp) {
+                break;
+            } else {
+                num = target;
+            }
+        }
+        bw.write(cnt + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
