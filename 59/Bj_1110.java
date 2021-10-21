1fa85933985740a94c410c3598a768ac4df8de5d
59 <5.9.dev@gmail.com>
2021-10-21 13:15:10 +0900

    Setting Complete

--- 59/Bj_1110.java
+++ 59/Bj_1110.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Bj_1110 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
